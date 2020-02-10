2a1f9d94b318e3290f4e1d844e3504f7dd6bee03
Temitope Akinsoto <takinsoto@gmail.com>
2020-02-10 17:37:30 +0100

    Print out even or odd depending on the users' input

--- src/10_functions.py
+++ src/10_functions.py
@@ -2,6 +2,12 @@
 
 # YOUR CODE HERE
 
+def is_even(number):
+    if number % 2 == 0:
+        return true
+    else:
+        return false
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
@@ -9,4 +15,8 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+if num % 2 == 0:
+    print("Even!")
+else:
+    print("Odd!")
 
