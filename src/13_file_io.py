6c06cb100717f76dc417b816ff4cd78a0bbde799
Temitope Akinsoto <takinsoto@gmail.com>
2020-02-11 00:32:46 +0100

    git read and write into a file

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,11 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+f = open('foo.txt', 'r')
+file_content = f.read()
+print(file_content)
+f.close()
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +22,6 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+f = open('bar.txt', 'w')
+f.write("Hello, world")
+f.close()
