c28fc40779173dcfca654f59bf316a4254831364
Temitope Akinsoto <takinsoto@gmail.com>
2020-02-11 14:43:27 +0100

    refactor module and printing exercise

--- src/04_printing.py
+++ src/04_printing.py
@@ -4,7 +4,7 @@ how to print using the printf operator, the `format` string
 method, and by using f-strings.
 """
 
-x = 10
+x = 10.25
 y = 2.24552
 z = "I like turtles!"
 
@@ -12,12 +12,12 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
-print("x is %r, y is %r, z is %s" % (x, round(y,2), z))
+print("x is %d, y is %.2f, z is \"%s\"" % (x, y, z))
 
 
 # Use the 'format' string method to print the same thing
-print("x is {}, y is {}, z is {} ".format(x, round(y,2), z))
+print("x is {:d}, y is {:.2f}, z is {} ".format(x, y, z))
 
 
 # Finally, print the same thing using an f-string
-print(f"x is {x}, y is {round(y,2)}, z is {z}")
+print(f"x is {x}, y is {y:.2f}, z is {z}")
