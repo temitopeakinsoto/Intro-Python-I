31c1ea63306d2264ca862f095c2a981f8fedfbe5
Temitope Akinsoto <takinsoto@gmail.com>
2020-02-12 00:59:50 +0100

    write a function that takes any number of variables

--- src/11_args.py
+++ src/11_args.py
@@ -15,6 +15,11 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*integers):
+    sum = 0
+    for integer in integers:
+        sum = sum + integer
+    return sum
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -24,7 +29,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
